979f15bf25dc47021efd0d495beae3118713220b
aa-zubkov <darenszx@bk.ru>
2020-03-03 22:15:35 +0300

    class Person result checking

--- src/com/sbt/jschool/Main.java
+++ src/com/sbt/jschool/Main.java
@@ -2,6 +2,18 @@ package com.sbt.jschool;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Person firstMan = new Person(true, "Nick");
+        Person firstWoman = new Person(false, "Alice");
+        Person secondMan = new Person(true, "Bob");;
+        Person secondWoman = new Person(false, "Jennifer");
+
+        System.out.println(firstMan.marry(secondMan));
+        System.out.println(firstWoman.marry(secondWoman));
+
+        System.out.println(firstMan.marry(firstWoman));
+        System.out.println(secondMan.marry(secondWoman));
+
+        System.out.println(firstMan.marry(secondWoman));
+        System.out.println(secondMan.marry(firstWoman));
     }
 }
